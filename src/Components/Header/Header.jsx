63baf29b16f8664733a4f2c83074d5a73b044cc2
Deptoha <tohakhan249156@gmail.com>
2024-05-28 13:48:28 +0600

    added responsive nav bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,36 +1,37 @@
-import { Link } from "react-router-dom";
-
-
-const links = <>
-    <Link to="/"><li className="text-base font-semibold"><a>Home</a></li></Link>
-    <Link to="/signIn"><li className="text-base font-semibold"><a>Sign In</a></li></Link>
-    <Link to="/register"><li className="text-base font-semibold"><a>Register</a></li></Link>
-
-</>
+import { useState } from 'react';
+import { BookOpenIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Header = () => {
+    let Links = [
+        { name: "HOME", link: "/" },
+        { name: "Sign In", link: "/signIn" },
+        { name: "Register", link: "/register" },
+        // { name: "CONTACT", link: "/" },
+    ];
+    let [open, setOpen] = useState(false);
+
     return (
-        <div>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {links}
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className='shadow-md w-full top-0 left-0 sticky'>
+            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+                {/* logo section */}
+                <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
+                    <BookOpenIcon className='w-7 h-7 text-blue-600' />
+                    <span>Inscribe</span>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {links}
-                    </ul>
+                {/* Menu icon */}
+                <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
+                    {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                {/* link items */}
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+                    {Links.map((link) => (
+                        <li key={link.name} className='md:ml-8 md:my-0 my-7 font-semibold'>
+                            <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
+                        </li>
+                    ))}
+                    <button className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>Get Started</button>
+                </ul>
+                {/* button */}
             </div>
         </div>
     );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,22 @@
 // import React from 'react';
 
+// import { Link } from "react-router-dom";
+
+
+// const Links = <>
+//     <Link to="/"><li className="text-base font-semibold"><a>Home</a></li></Link>
+//     <Link to="/signIn"><li className="text-base font-semibold"><a>Sign In</a></li></Link>
+//     <Link to="/register"><li className="text-base font-semibold"><a>Register</a></li></Link>
+
+// </>
+
+import './home.css'
 import Login from "../Login/Login";
 
 const Home = () => {
     return (
         <div>
-            <div className="text-4xl font-bold my-14 text-center">
+            <div className="text-4xl font-bold mt-32 mb-12 text-center">
                 This is home
             </div>
 
